f29b74bf01cd33bca2ae7f3e903fbfb708b24793
lucakorosec <korosecluca@gmail.com>
2021-03-19 15:38:48 +0100

    String Concatenation

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,4 +1,2 @@
-console.log(25+3.5) //This is the age you’ll be when we start sending people to live on Mars.
-console.log(2021-1969) //The answer is how many years it’s been since the 1969 moon landing.
-console.log(65/240)
-console.log(0.2708*100) /*That’s the percent of the sun that is made up of helium. Assuming we could stand on the sun, we’d all sound like chipmunks! */
+console.log('Hello'+'World')
+console.log('Hello'+' '+'World')
