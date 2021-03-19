fc1df8e0c8e5b407c3700f3a318bb9db511b2270
lucakorosec <korosecluca@gmail.com>
2021-03-19 16:59:58 +0100

    typeof operator

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,3 +1,4 @@
-const myName = 'Luca';
-const myCity = 'Rome';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable);
