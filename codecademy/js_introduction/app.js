4db65c6bf3e82e243528fb65d3af196d62e6373c
lucakorosec <korosecluca@gmail.com>
2021-03-19 15:34:07 +0100

    Arithmetic Operators

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,4 +1,4 @@
-console.log('JavaScript')
-console.log(2011)
-console.log('Woohoo! I love to code! #codecademy')
-console.log(20.49)
+console.log(25+3.5) //This is the age you’ll be when we start sending people to live on Mars.
+console.log(2021-1969) //The answer is how many years it’s been since the 1969 moon landing.
+console.log(65/240)
+console.log(0.2708*100) /*That’s the percent of the sun that is made up of helium. Assuming we could stand on the sun, we’d all sound like chipmunks! */
