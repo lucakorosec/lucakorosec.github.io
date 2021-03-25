10cf16fe3a8906f3a8afac6e629e2004b6f98b2e
lucakorosec <korosecluca@gmail.com>
2021-03-25 16:16:54 +0100

    karte erstellt

--- nz/main.js
+++ nz/main.js
@@ -1,4 +1,12 @@
 
 console.log(L);
-const map = L.map("map");
+
+const map = L.map("map", {
+    center: [-43.5, 170.22],
+    zoom: 13,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{z}.png")
+    ]
+});
+
 console.log(document.querySelector("#map")); /* raute map sucht mir das element mit der ID = Map)*/
