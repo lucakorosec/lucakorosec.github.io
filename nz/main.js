e74236e034e6b3a6bff6edd79d5a25e53d32dbdb
lucakorosec <korosecluca@gmail.com>
2021-03-25 16:20:12 +0100

    marker hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -2,11 +2,13 @@
 console.log(L);
 
 const map = L.map("map", {
-    center: [-43.5, 170.22],
+    center: [-43.4668631325, 170.188249247],
     zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{z}.png")
     ]
 });
+
+let mrk = L.marker([-43.4668631325, 170.188249247]).addTo(map);
 
 console.log(document.querySelector("#map")); /* raute map sucht mir das element mit der ID = Map)*/
