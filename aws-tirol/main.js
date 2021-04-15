71ed1255cc156110635a7d5ff76136eb2c6dd54b
lucakorosec <korosecluca@gmail.com>
2021-04-15 16:12:06 +0200

    weitere basekarten hinzugefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -14,5 +14,9 @@ let map = L.map ("map", {
 //L greift auf leaflet zu, control den script zu controls in der L bibliothek und layers sag i selber
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
-    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
-}).addTo(map); //zur karte hinzufügen. muss bei L passieren
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.terrain": L.tileLayer.provider('BasemapAT.terrain'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.overlay": L.tileLayer.provider('BasemapAT.overlay'),
+    "BasemapAT.basemap": L.tileLayer.provider('BasemapAT.basemap'),
+}).addTo(map); //zur karte hinzufügen. muss bei L passieren am ende von der schleife
