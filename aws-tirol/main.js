24076ec87a1260940220a14ad0f604b2da55552f
lucakorosec <korosecluca@gmail.com>
2021-04-15 16:02:59 +0200

    add baselayr plugin & control pannel

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,8 +1,18 @@
 
+let basemapGray = L.tileLayer.provider('BasemapAT.grau');
+
 let map = L.map ("map", {
     center: [47, 11],
     zoom: 9,
-    layer: [
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    layers: [
+       // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        basemapGray
     ]
-})
+});
+
+
+//L greift auf leaflet zu, control den script zu controls in der L bibliothek und layers sag i selber
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto')
+}).addTo(map); //zur karte hinzufügen. muss bei L passieren
