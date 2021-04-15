5617f2b468e98f7dede5648a26997c7b0242002c
lucakorosec <korosecluca@gmail.com>
2021-04-15 16:42:43 +0200

    add fetch for geodata from ogd

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -17,6 +17,30 @@ let layerControl = L.control.layers({
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
     "BasemapAT.terrain": L.tileLayer.provider('BasemapAT.terrain'),
     "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
-    "BasemapAT.overlay": L.tileLayer.provider('BasemapAT.overlay'),
+    //"BasemapAT.overlay": L.tileLayer.provider('BasemapAT.overlay'),
     "BasemapAT.basemap": L.tileLayer.provider('BasemapAT.basemap'),
+    //das erste rote ist was dann auf der seite im drop down steht
+    //layer kombinieren
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ])
 }).addTo(map); //zur karte hinzufügen. muss bei L passieren am ende von der schleife
+
+
+let awsURL = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+// mit let die varibael benannt in awsurl, mit fetch soll er die daten aus datagvat laden, mit then soll er eine "response" < so nennen wir es, sagen dass es geladen ist
+fetch(awsURL)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertiert: ', json);
+        for (station of json.features) {
+            console.log('Station: ', station);
+            let marker  = L.marker(
+                [station.geometry.coordinates[1],
+                station.geometry.coordinates[0]]
+                );
+            marker.addTo(map);
+        }
+});
