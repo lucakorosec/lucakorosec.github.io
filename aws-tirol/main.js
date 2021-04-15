78246c7ce69ac0742d00784f4f2cffb16951ac4d
lucakorosec <korosecluca@gmail.com>
2021-04-15 17:02:39 +0200

    kommentare

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -10,7 +10,7 @@ let map = L.map ("map", {
     ]
 });
 
-
+// baselayer von der provider seite auf unsere seite reinladen https://cdnjs.com/libraries/leaflet-providers
 //L greift auf leaflet zu, control den script zu controls in der L bibliothek und layers sag i selber
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
@@ -30,17 +30,21 @@ let layerControl = L.control.layers({
 
 let awsURL = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
-// mit let die varibael benannt in awsurl, mit fetch soll er die daten aus datagvat laden, mit then soll er eine "response" < so nennen wir es, sagen dass es geladen ist
-fetch(awsURL)
-    .then(response => response.json())
-    .then(json => {
+// mit let die varibael benannt in awsurl, mit fetch soll er die daten aus datagvat laden, mit then soll er eine "response" < so nennen wir es, sagen dass es geladen und eine json ist
+//und mit console.log in der f12 console soll stehen wenn die daten als json erkannt und geladen worden sind in zeile 1 schreiben
+//for jede station in der json datenbank (sind darin als feature bezeichnet) soll er in der f12 console station schreiben
+// und mit let marker einen marker in der karte darstellen mit den coord aus der json datenbank die wir ja station genannt haben, daher station.geometry (geometry steht wieder in der json datenbank) und dann 0 für long und 1 lat
+
+fetch(awsURL) //daten herunterladen von der datagvat bib
+    .then(response => response.json()) //konvertieren in json (fehleranfällig daher nächste then clause)
+    .then(json => { //weiterarbeiten mit json
         console.log('Daten konvertiert: ', json);
         for (station of json.features) {
             console.log('Station: ', station);
             let marker  = L.marker(
                 [station.geometry.coordinates[1],
-                station.geometry.coordinates[0]]
-                );
+                station.geometry.coordinates[0]
+            ]);
             marker.addTo(map);
         }
 });
