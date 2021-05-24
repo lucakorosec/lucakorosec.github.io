85d229d9af1f1283391e63bef2b622a0eac0c791
lucakorosec <korosecluca@gmail.com>
2021-05-24 15:42:51 +0200

    test wms einbindung

---
+++ projekt/main.js
@@ -0,0 +1,42 @@
+let baselayers = {
+    standard: L.tileLayer.provider("BasemapAT.basemap"),
+    grau: L.tileLayer.provider("BasemapAT.grau"),
+    terrain: L.tileLayer.provider("BasemapAT.terrain"),
+    surface: L.tileLayer.provider("BasemapAT.surface"),
+    highdpi: L.tileLayer.provider("BasemapAT.highdpi"),
+    wms: L.tileLayer.wms('https://maps.tirol.gv.at/externalcall.jsp?user=guest&project=tmap_master&view=sport_wintersport', {
+        layers: 'PVECT\PLA_RO\SKJ_PL'
+    }),
+    ortho_overlay: L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ]),
+};
+
+// Overlays für die Themen zum Ein- und Ausschalten definieren
+let overlays = {
+    tracks: L.featureGroup()
+};
+
+// Karte initialisieren und auf Innsbrucks Wikipedia Koordinate blicken
+let map = L.map("map", {
+    center: [47.267222, 11.392778],
+    zoom: 9,
+    layers: [
+        baselayers.grau
+    ]
+})
+
+// Kartenhintergründe und Overlays zur Layer-Control hinzufügen
+let layerControl = L.control.layers({
+    "basemap.at Standard": baselayers.standard,
+    "basemap.at grau": baselayers.grau,
+    "basemap.at Relief": baselayers.terrain,
+    "basemap.at Oberfläche": baselayers.surface,
+    "basemap.at hochauflösend": baselayers.highdpi,
+    "basemap.at Orthofoto beschriftet": baselayers.ortho_overlay,
+}, {
+    "WMS": baselayers.wms
+}).addTo(map);
+
+console.log(baselayers.wms);
