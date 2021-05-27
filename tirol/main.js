238d7ce31d3aed34ca630759a1f4a872840d0cf6
lucakorosec <korosecluca@gmail.com>
2021-05-27 17:43:39 +0200

    weblink als neuen baustein definieren

--- tirol/main.js
+++ tirol/main.js
@@ -191,6 +191,7 @@ const updateTexts = (nr) => {
         // ist es die aktuelle etappe ?`
         if (etappe.nr == nr) {
             //console.log("unsere Etappe", etappe)
+            etappe.homepage = `<a href="${etappe.weblink}">Homepage</a>`
             for (let key in etappe) {
                 console.log("key: ", key, "value: ", etappe[key]);
                 // gibt es ein element im html mit der ID von "key"
